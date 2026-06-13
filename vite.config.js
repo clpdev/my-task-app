27a31adaebdd7ce9e4cd4298e31833101eb9c65a
areki <areki@example.com>
2026-06-13 17:53:40 +0900

    alpha0.0.0

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    // 本番ビルド時のみリポジトリ名をベースパスにする
+    base: command === 'build' ? '/my-task-app/' : '/',
+  }
 })
